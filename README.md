802578b6fbe00555238a67c6fe35fc71eb70d720
statho7 <andreas.stathop@gmail.com>
2021-08-09 22:37:03 +0100

    Added Merge() function for join on pandas dataframes and added the instructions on the README file

--- README.md
+++ README.md
@@ -2,10 +2,19 @@
 
 ## export_csv_from_multiple_csv_files.py
 
-To create a combined csv file from multiple csv files run in your command line 
+## Concatenate rows of multiple CSV files to one CSV file
+To create a combined csv file from multiple csv files run in your command line:
 
-python export_csv_from_multiple_csv_files.py {name of the csv file to create} {csv file 1} {csv file 2} {csv file 3} etc.
+python export_csv_from_multiple_csv_files.py concatenate {name of the csv file to create} {csv file 1} {csv file 2} {csv file 3} etc.
 
 ### Example
 
-python export_csv_from_multiple_csv_files.py C:/Users/DataViz/Combined_Stats.csv C:/Users/DataViz/Births.csv C:/Users/DataViz/Deaths.csv C:/Users/DataViz/Population.csv
+python export_csv_from_multiple_csv_files.py concatenate C:/Users/DataViz/Combined_Stats.csv C:/Users/DataViz/Births.csv C:/Users/DataViz/Deaths.csv C:/Users/DataViz/Population.csv
+
+## Join CSV files
+
+To join two or multiple CSV files run in your command line:
+
+python export_csv_from_multiple_csv_files.py merge {name of the csv file to create} {column} {type of join (left, right, outer, inner, cross)} {csv file 1} {csv file 2} {csv file 3} etc.
+
+python export_csv_from_multiple_csv_files.py merge C:/Users/DataViz/Combined_Stats.csv country_id left C:/Users/DataViz/Births.csv C:/Users/DataViz/Deaths.csv C:/Users/DataViz/Population.csv
--- export_csv_from_multiple_csv_files.py
+++ export_csv_from_multiple_csv_files.py
@@ -2,6 +2,12 @@ import sys
 import pandas as pd
 import os
 
+def Run(action, *args):
+    if action == 'merge':
+        Merge(args)
+    elif action == 'concatenate':
+        Create_the_combined_csv_file(args)
+
 def Create_the_combined_csv_file(export_filename, *args):
 
     # CSV to Pandas DataFrame the first 2 CSV files
@@ -23,9 +29,32 @@ def Create_the_combined_csv_file(export_filename, *args):
     # After appending all the Pandas DataFrame to the export_csv DataFrame we are exporting it to a CSV file with the name mentioned in the command line
     export_csv.to_csv(f'{export_filename}', index=False)
 
+def Merge(export_filename, column, join, *args):
+
+    # CSV to Pandas DataFrame the first 2 CSV files
+    csv_1 = pd.read_csv(f'{args[0][0]}')
+    csv_2 = pd.read_csv(f'{args[0][1]}')
+
+    # Append the first csv to the other
+    export_csv = csv_1.merge(csv_2, on=column, how=join)
+
+    # If more than 2 CSV files have been mentioned in command line this will run
+    try:
+        # For every extra csv file we are creating a Pandas DataFrame and then append it to the existing one to export
+        for arg in args[0][2:]:
+            csv_2 = pd.read_csv(f'{arg}')
+            export_csv = export_csv.merge(csv_2, on=column, how=join)
+    except:
+        pass
+
+    # After appending all the Pandas DataFrame to the export_csv DataFrame we are exporting it to a CSV file with the name mentioned in the command line
+    export_csv.to_csv(f'{export_filename}', index=False)
+
 if __name__ == '__main__':
     # Install pandas if you do not have already
     os.system("pip install pandas")
-    
-    # str(sys.argv[1]) is the path we want to save our combined csv file and sys.argv[2:] the paths of the csv files we want to combine
-    Create_the_combined_csv_file(str(sys.argv[1]),sys.argv[2:])
+
+    try:
+        Run(str(sys.argv[1]),str(sys.argv[2]),sys.argv[3:])
+    except:
+        print('You should state more arguments')
